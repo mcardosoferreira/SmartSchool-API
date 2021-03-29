800ea5462bf3774c6ec5c38ea35a1b113bf60ca7
Matheus <matheus_master08@hotmail.com>
2021-03-29 15:44:35 -0300

    Modificação do controller de aluno para receber o Context

--- SmartSchool.API/Controller/AlunoController.cs
+++ SmartSchool.API/Controller/AlunoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SmartSchool.API.Context;
 using SmartSchool.API.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -13,50 +14,30 @@ namespace SmartSchool.API.Controller
     [ApiController]
     public class AlunoController : ControllerBase
     {
-        public List<Aluno> Alunos = new List<Aluno>()
-       {
-           new Aluno()
-           {
-               Id = 1,
-               Nome = "Marcos",
-               Sobrenome = "assunção",
-               Telefone = "123154"
-           },
-           new Aluno()
-           {
-               Id = 2,
-               Nome = "Marta",
-               Sobrenome = "Santos",
-               Telefone = "1231542"
-           },
-           new Aluno()
-           {
-               Id = 3,
-               Nome = "Laura",
-               Sobrenome = "Almeida",
-               Telefone = "1231543"
-           }
-       };
-        public AlunoController() { }
+        private readonly SmartContext _context;
 
+        public AlunoController(SmartContext context)
+        {
+            _context = context;
+        }
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(Alunos);
+            return Ok(_context.Alunos);
         }
-        [HttpGet("byId")]
+        [HttpGet("ById")]
         public IActionResult GetById(int id)
         {
-            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Id == id);
             if (aluno == null)
                 return BadRequest("Aluno não encontrado");
             return Ok(aluno);
         }
         
-        [HttpGet("byName")]
+        [HttpGet("ByName")]
         public IActionResult GetByName(string nome, string sobrenome)
         {
-            var aluno = Alunos.FirstOrDefault(a => a.Nome.Contains(nome) && a.Sobrenome.Contains(sobrenome));
+            var aluno = _context.Alunos.FirstOrDefault(a => a.Nome.Contains(nome) && a.Sobrenome.Contains(sobrenome));
             if (aluno == null)
                 return BadRequest("Aluno não encontrado");
             return Ok(aluno);
