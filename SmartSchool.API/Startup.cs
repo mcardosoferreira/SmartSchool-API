0daaa77fc713c28d4b75f4eb3b271c30d9c46ad4
Matheus <matheus_master08@hotmail.com>
2021-03-29 15:13:22 -0300

    Criação e configuração do SmartContext

--- SmartSchool.API/Startup.cs
+++ SmartSchool.API/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SmartSchool.API.Context;
 
 namespace SmartSchool.API
 {
@@ -24,6 +26,9 @@ namespace SmartSchool.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SmartContext>(
+                context => context.UseSqlite(Configuration.GetConnectionString("Default"))
+                ); 
             services.AddControllers();
         }
 
